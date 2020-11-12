78f2ae2b16e86847ccbbf48c6f21649c7ed61869
vikuviku6666 <53383503+vikuviku6666@users.noreply.github.com>
2020-11-12 17:43:06 +0100

    componenets added
    new components files added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-webshop-app'`, () => {
+  it(`should have as title 'my-webShop-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-webshop-app');
+    expect(app.title).toEqual('my-webShop-app');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CategoriesComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    SearchComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
